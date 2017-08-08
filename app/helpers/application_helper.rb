8e13edad86c035c5ec0c5f72ada66377791bc85c
Francois Catuhe <fcatuhe@gmail.com>
2017-08-08 20:35:46 +0200

    implement meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def yield_with_default(holder, default)
+    if content_for?(holder)
+      str = content_for(holder).squish
+      str += " | #{default}" if holder == :title
+      str
+    else
+      default
+    end
+  end
 end
