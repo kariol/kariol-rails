c3032a56a40bca8c62f8891fa4438695cac7db10
Francois Catuhe <fcatuhe@gmail.com>
2017-08-29 09:23:16 +0200

    add link to enterprise, add link to root on logo

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
 
   root to: 'pages#home'
-  get 'entreprise', to: 'pages#enterprise'
+  get 'entreprise', to: 'pages#enterprise', as: :enterprise
   get 'legal', to: 'pages#legal'
 
   resources :commute_requests, only: [ :create ]
