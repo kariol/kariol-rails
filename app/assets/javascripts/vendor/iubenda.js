5cb622011aff6eb91dce2acd3dccb47516313794
Francois Catuhe <fcatuhe@gmail.com>
2017-09-14 15:32:47 +0200

    add hotjar in production

--- app/assets/javascripts/vendor/iubenda.js
+++ app/assets/javascripts/vendor/iubenda.js
@@ -1,14 +1,18 @@
-(function (w,d) {
-  var loader = function () {
-    var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0];
-    s.src = "//cdn.iubenda.com/iubenda.js";
-    tag.parentNode.insertBefore(s,tag);
+document.addEventListener('turbolinks:load', function() {
+  if (document.querySelector(".pages.legal")) {
+    (function (w,d) {
+      var loader = function () {
+        var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0];
+        s.src = "//cdn.iubenda.com/iubenda.js";
+        tag.parentNode.insertBefore(s,tag);
+      };
+      if(w.addEventListener){
+        w.addEventListener("turbolinks:load", loader, false);
+      }else if(w.attachEvent){
+        w.attachEvent("onload", loader);
+      }else{
+        w.onload = loader;
+      }
+    })(window, document);
   };
-  if(w.addEventListener){
-    w.addEventListener("turbolinks:load", loader, false);
-  }else if(w.attachEvent){
-    w.attachEvent("onload", loader);
-  }else{
-    w.onload = loader;
-  }
-})(window, document);
+});
