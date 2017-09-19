959dffc3dc9893cd4853e70e9b6174db6039fa5a
Francois Catuhe <fcatuhe@gmail.com>
2017-09-19 11:04:13 +0200

    add pro page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,6 @@
 //= require turbolinks
 //= require jquery
 //= require bootstrap-sprockets
+//= require mapbox-gl/dist/mapbox-gl
+//= require mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min
 //= require_tree .
---
+++ app/assets/javascripts/mapgox_gl.js
@@ -0,0 +1,18 @@
+document.addEventListener('turbolinks:load', function() {
+  if (document.querySelector('.pages.pro')) {
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGFkYW1pbyIsImEiOiJjajE1MHpvaDgwMDBxMnFuNW16cjFwZXNhIn0.SrEc4qX1z94maLdY-XisZQ';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/padamio/cj15101qx002d2smfqddmvtdx',
+      center: [2.019,48.793],
+      zoom: 10,
+      scrollZoom: false
+    });
+
+    map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      country: 'FR',
+      placeholder: 'Recherche'
+    }));
+  };
+});
